be799edda9a946d6768eadac72869052911eb300
Mariana Valencia <purple.mv@gmail.com>
2020-02-09 19:31:14 -0500

    Searching bar

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -6,7 +6,21 @@ import confLogo from '../images/platziconf-logo.svg';
 import Badge from '../components/Badge';
 import DeleteBadgeModal from '../components/DeleteBadgeModal';
 
+function useIncreaseCount(max) {
+    const [ count, setCount ] = React.useState(0);
+
+    if(count > max) {
+        setCount(0);
+    }
+
+    return [count, setCount];
+}
+//Esto es un hook personalizado porque YOLO
+
 function BadgeDetails (props) {
+    const [ count, setCount ] = useIncreaseCount(4);
+    //HOOKS: esto es como state / setstate excepto que el nombre lo damos. El argumento opcional que le damos a useState es el valor inicial de la variable 
+
     const badge = props.badge;
     return (
         <div>
@@ -31,6 +45,11 @@ function BadgeDetails (props) {
                         <h2>Actions</h2>
                         <div>
                             <div>
+                                <button onClick={() => {
+                                    setCount(count + 1);
+                                }} className="btn btn-primary mr-4">
+                                    Increase Count {count}
+                                </button>
                                 <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>Edit</Link>
                             </div> 
                             <div>
